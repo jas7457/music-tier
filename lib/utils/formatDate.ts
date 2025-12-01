02b7507492fa114dabded01c7e7f1f57afcf651b
Jason Addleman <jas7457@gmail.com>
2025-11-30 19:45:34 -0500

    Get rid of year

--- lib/utils/formatDate.ts
+++ lib/utils/formatDate.ts
@@ -3,21 +3,11 @@ export function formatDate(
   options: Intl.DateTimeFormatOptions = {}
 ) {
   const date = new Date(timestamp);
-  const now = new Date();
   const timeZone = "America/New_York";
-
-  // Use Intl to get the year *in Eastern time* for both dates
-  const yearFormatter = new Intl.DateTimeFormat("en-US", {
-    timeZone,
-    year: "numeric",
-  });
-
-  const sameYearInET = yearFormatter.format(date) === yearFormatter.format(now);
 
   return new Intl.DateTimeFormat("en-US", {
     month: "short",
     day: "numeric",
-    ...(sameYearInET ? {} : { year: "numeric" }),
     ...options,
     // Ensure EST/EDT no matter where this code runs
     timeZone,
