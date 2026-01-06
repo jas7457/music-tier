2ddfa93ed73d00f9c7d2f6e2f27125ad0a7fad38
Jason Addleman <jas7457@gmail.com>
2026-01-05 21:08:32 -0500

    Better icon

--- components/playback/components/DualScreen.tsx
+++ components/playback/components/DualScreen.tsx
@@ -63,12 +63,11 @@ export function DualScreen({
             stroke="currentColor"
             className="w-6 h-6"
           >
-            {/* Info circle */}
-            <circle cx="12" cy="12" r="10" />
+            {/* Flip/Rotate icon - two curved arrows suggesting flip */}
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M12 16v-4m0-4h.01"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
             />
           </svg>
         </button>
