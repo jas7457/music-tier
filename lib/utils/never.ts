732e5111708aefd8a42b94029b557a8a0a0dcb42
Jason Addleman <jas7457@gmail.com>
2026-01-25 21:28:29 -0500

    whatever your commit message is

--- lib/utils/never.ts
+++ lib/utils/never.ts
@@ -1,7 +1,9 @@
 export function assertNever(x: never): never {
-  throw new Error('Unexpected object: ' + x);
+  throw new Error("Unexpected object: " + x);
 }
 
 export function assumeNever(x: never): never {
   return x;
 }
+
+console.log("hey there");
